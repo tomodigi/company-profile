a4df45e53b557a9cb179acd03f647bdd080d0894
Andre Saputra <andre.saputra@lolipad.com>
2023-02-02 20:05:52 +0700

    add jumbotron

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -2,6 +2,6 @@
 </script>
 <template>
   <div class="p-5">
-    <h1>Hello Nuxt</h1>
+    <h1 class="font-tomo-main">Digital Creative</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // ssr: true,
+  app: {
+    head: {
+      title: 'Tomo Digital Creative',
+      htmlAttrs: { lang: 'en' },
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        }
+      ],
+      link: [
+        {
+          rel: 'icon',
+          href: '/favicon.png'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Kanit:wght@600;700&family=Plus+Jakarta+Sans&display=swap'
+        }
+      ]
+    }
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
