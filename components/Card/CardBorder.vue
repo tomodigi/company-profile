49756b10bd8c370d775debfcd616118876867615
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2024-09-29 14:46:06 +0700

    ✨ Add services

--- components/Card/CardBorder.vue
+++ components/Card/CardBorder.vue
@@ -1,10 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+export interface CardProps {
+  imgSrc: string;
+  title: string;
+  description: string;
+}
+
+const props = defineProps<CardProps>();
+</script>
+
 <template>
-  <div id="card" class="flex flex-col gap-8 p-10 border border-gray-400 rounded-xl">
-    <img src="/vision_1.png" alt="" width="50">
+  <div
+    id="card"
+    class="flex flex-col gap-8 p-10 border border-gray-400 rounded-xl"
+  >
+    <img :src="imgSrc" alt="" width="50" />
     <div class="w-[40%] border" />
-    <h3 class="text-xl font-semibold">Visual Identity</h3>
-    <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <button type="button" class="px-4 py-2 text-sm font-medium rounded-full bg-gradient-primary w-fit">Contact Us</button>
+    <h3 class="text-xl font-semibold">{{ title }}</h3>
+    <p class="text-sm">{{ description }}</p>
+    <button
+      type="button"
+      class="px-4 py-2 text-sm font-medium rounded-full bg-gradient-primary w-fit"
+    >
+      Contact Us
+    </button>
   </div>
 </template>
--- components/content/SectionServices.vue
+++ components/content/SectionServices.vue
@@ -1,21 +1,73 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { CardProps } from "./../Card/CardBorder.vue";
+import CardBorder from "./../Card/CardBorder.vue";
+
+const cardsData: CardProps[] = [
+  {
+    imgSrc: "/service/visual-identity.png",
+    title: "Visual Identity",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    imgSrc: "/service/social-media-management.png",
+    title: "Social Media Management",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    imgSrc: "/service/web-development.png",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    imgSrc: "/service/photography.png",
+    title: "Photography",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    imgSrc: "/service/videography.png",
+    title: "Videography",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    imgSrc: "/service/motion-graphic.png",
+    title: "Motion Graphic",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    imgSrc: "/service/3d-animation.png",
+    title: "3D Animation",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    imgSrc: "/service/design-exterior.png",
+    title: "Design Exterior",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    imgSrc: "/service/digital-marketing.png",
+    title: "Digital Marketing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+</script>
+
 <template>
-  <section id="services" class="flex flex-col items-center justify-center w-full p-10 rounded-xl bg-background-base">
-    <h2
-      class="section--title w-full xl:w-[850px] text-center"
-      data-aos="fade-up"
-      data-aos-duration="1000"
-    >
-      <slot name=title></slot>
-    </h2>
-    <div class="grid gap-10 mt-24 sm:grid-cols-2 xl:grid-cols-3">
-      <CardBorder
-        v-for="(item, index) in 10"
-        :key="index"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-        :data-aos-delay="150 + (index * 100)"
-      />
-    </div>
-  </section>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <CardBorder
+      v-for="(card, index) in cardsData"
+      :key="index"
+      :title="card.title"
+      :description="card.description"
+      :imgSrc="card.imgSrc"
+    />
+  </div>
 </template>
