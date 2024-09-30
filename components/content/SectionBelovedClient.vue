7b89de0a405b7c09e339ccc193f0b9696aee00f1
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2024-09-30 10:54:16 +0700

    ✨ Add our beloved client section

---
+++ components/content/SectionBelovedClient.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+const clients = [
+  {
+    name: "Couple Project",
+    img: "/client/couple-project.jpg",
+  },
+  {
+    name: "Fadsan",
+    img: "/client/fadsan.jpg",
+  },
+  {
+    name: "Kohesif Studio",
+    img: "/client/kohesif-studio.jpg",
+  },
+  {
+    name: "Masfree Studio",
+    img: "/client/masfree-studio.jpg",
+  },
+  {
+    name: "RJU",
+    img: "/client/rju.png",
+  },
+  {
+    name: "WLS",
+    img: "/client/wls.jpg",
+  },
+  {
+    name: "Goresan Studio",
+    img: "/client/goresan-studio.png",
+  },
+];
+</script>
+<template>
+  <div>
+    <div class="max-w-4xl mx-auto">
+      <img
+        src="/logo-white.png"
+        alt="Logo White"
+        width="50"
+        height="50"
+        class="mx-auto"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      />
+      <h2
+        class="section--title shrink-0 text-center mt-5 mb-10"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      >
+        <slot name="title"></slot>
+      </h2>
+    </div>
+    <hr
+      class="border border-gray-500 mb-16"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+    />
+    <div
+      class="grid grid-cols-7 gap-5"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+    >
+      <div
+        v-for="(client, index) in clients"
+        :key="index"
+        class="flex items-center justify-center"
+      >
+        <div class="text-gray-400 flex items-center space-x-2">
+          <img :src="client.img" :alt="client.name" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- content/index.md
+++ content/index.md
@@ -1,18 +1,22 @@
 ::section-jumbotron
 #title
-[Digital]{style="color: #9762FD"} Creative Partner for Innovative [Solution]{style="color: #9762FD"}
+[Digital]{style="color: #C0ADFF"} Creative Partner for Innovative [Solution]{style="color: #C0ADFF"}
 #subtitle
 Tomodigi.id adalah sebuah agensi digital kreatif yang berfokus pada pengembangan solusi digital inovatif untuk membantu brand dan bisnis berkembang di era digital. lebih dari 50+ project yang sudah kami selesaikan dengan pendekatan yang disesuaikan dan inovatif.
 ::
 ::section-introduction
 #title
-Jelajahi Dunia Kreatif Digital dengan [Tomodigi.id]{style="color: #9762FD"}
+Jelajahi Dunia Kreatif Digital dengan [Tomodigi.id]{style="color: #C0ADFF"}
 #subtitle
 Raih audiens yang lebih luas dan bangun hubungan yang kuat dengan pelanggan.
 #video-title
 Intro Video
 #video-subtitle
 Dengan Tomodigi.id, wujudkan ide-ide besar Anda di dunia digital
+::
+::section-beloved-client
+#title
+Our Beloved [Client]{style="color: #C0ADFF"}
 ::
 ::section-services
 #title-1
@@ -22,7 +26,7 @@ we believe in
 ::
 ::section-recent-work
 #title
-Our Most Recent [Work]{style="color: #9762FD"}
+Our Most Recent [Work]{style="color: #C0ADFF"}
 #subtitle
 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid neque unde incidunt aspernatur nulla quos?
 ::
